c2000f453aa53b69b31e6d182f49f54ed9326b1b
Frn-sz <fernandogdemedeirosw@gmail.com>
2024-10-29 20:48:52 -0300

    build: added users migration

---
+++ src-tauri/migration/src/m20241029_231516_create_users_table.rs
@@ -0,0 +1,35 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let table = Table::create()
+            .table(User::Table)
+            .if_not_exists()
+            .col(pk_auto(User::Id))
+            .col(string(User::Name))
+            .col(string(User::Email))
+            .col(string(User::Password))
+            .to_owned();
+
+        manager.create_table(table).await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    Name,
+    Id,
+    Email,
+    Password,
+}

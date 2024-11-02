a14e1c9849da14df4372eb8434bd7bf6d9857abd
Frn-sz <fernandogdemedeirosw@gmail.com>
2024-11-02 16:48:42 -0300

    refactor: move entity to src-tauri/src

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+mod entity;
+
+use entity::prelude::*;
+fn main() {
+    tauri_to_do_lib::run()
+}
